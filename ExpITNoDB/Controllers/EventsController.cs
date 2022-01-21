4ccb7e0d7e7154b1df21beefd3062b2c0a523f68
kalai <skalaiarasy@gmail.com>
2022-01-21 14:33:12 -0500

    Add project files.

---
+++ ExpITNoDB/Controllers/EventsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ExpITNoDB.Controllers
+{
+    public class EventsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            
+            string apiUrl = "https://localhost:44394/api/EventMasters";
+
+            var result = await client.GetStringAsync(apiUrl);
+
+
+            return View(result);
+        }
+    }
+}
